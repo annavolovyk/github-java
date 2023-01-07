7404864a76ea86749d8122663737972f05430dc1
Anna Volovyk <volovyk.anna@gmail.com>
2023-01-07 19:18:36 +0200

    Added second try

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Nice to meet you world. This my first text to publish");
+        System.out.println("This is our second commit");
     }
 }
